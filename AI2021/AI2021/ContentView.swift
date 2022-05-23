57b6066ce23290bc41d2319da6a6f1425eb152fa
muskaansatsangi <106054165+muskaansatsangi@users.noreply.github.com>
2022-05-23 01:37:53 -0500

    Add files via upload

---
+++ AI2021/AI2021/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  AI2021
+//
+//  Created by Satweka Nammi on 3/28/21.
+//  Copyright © 2021 Satweka Nammi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ AI2021/AI2021Tests/AI2021Tests.swift
@@ -0,0 +1,34 @@
+//
+//  AI2021Tests.swift
+//  AI2021Tests
+//
+//  Created by Satweka Nammi on 3/28/21.
+//  Copyright © 2021 Satweka Nammi. All rights reserved.
+//
+
+import XCTest
+@testable import AI2021
+
+class AI2021Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
